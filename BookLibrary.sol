9381dc7f697c6fff3637ff7c6b8eb5752207f020
Rosen Santev <r.ssantev@gmail.com>
2021-10-11 02:06:02 +0300

    Refactoring
    1. Change namings
    2. Update documentation
    3. Update code styling

--- BookLibrary.sol
+++ BookLibrary.sol
@@ -19,46 +19,35 @@ contract BookLibrary is Ownable{
     // The array is public, so solidity automatically creates a getter function for the array's elements (element's index has to be provided)
     Book[] public books;
     // Shows is a title is being added in the library
-    mapping(string => bool) isAdded;
+    mapping(string => bool) isBookAdded;
 
     address[] borrowers;
+    // Shows is a sender is already a borrower
     mapping(address => bool) isBorrower;
     // Shows which books a user is borrowed (user => (bookId => bool)
     mapping(address => mapping(uint => bool)) userBorrowed;
 
-    function addBorrower(address _borrower) private {
-        if(!isBorrower[_borrower]) {
-            borrowers.push(_borrower);
-            isBorrower[_borrower] = true;
-        }
-
-    }
-
     /**
-     * @dev Add new book with ('_title') and ('copiesCount') in the books array.
+     * @notice Add new book with ('_title') and ('_copiesCount') in the books array.
      * Can only be called by the current owner.
      * Can only be invoked outside the contract.
+     * @param _title - the title of the book
+     * @param _copiesCount -  the number of copies of this book
      */
     function addBook(string memory _title, uint _copiesCount) external onlyOwner {
-        require(!isAdded[_title], "This book is already added!");
+        require(!isBookAdded[_title], "This book is already added!");
 
         books.push(Book(_title, _copiesCount));
-        isAdded[_title] = true;
+        isBookAdded[_title] = true;
         uint id = books.length - 1;
 
         emit BookAdded(id, _title, _copiesCount);
     }
-
+    
     /**
-     * @dev Returns all added books and their copies number.
-     */
-    function viewAllBooks() public view returns ( Book[] memory) {
-        return books;
-    }
-
-    /**
-     * @dev Borrow a book by ('_bookId').
+     * @notice Borrow a book by ('_bookId').
      * Can only be invoked outside the contract.
+     * @param _bookId - the ID of the book
      */
     function borrowBook(uint _bookId) external {
         require(books[_bookId].copies > 0, "There are no copies left! Try again later.");
@@ -73,8 +62,9 @@ contract BookLibrary is Ownable{
     }
 
     /**
-     * @dev Return a book by ('_bookId').
+     * @notice The borrower returns a book by ('_bookId').
      * Can only be invoked outside the contract.
+     * @param _bookId - the ID of the book
      */
     function returnBook(uint _bookId) external {
         require(userBorrowed[msg.sender][_bookId] == true, "You don't own a copy of this book.");
@@ -85,8 +75,32 @@ contract BookLibrary is Ownable{
         emit BookReturned(_bookId, msg.sender);
     }
 
+    /**
+     * @notice Returns all added books and their copies number.
+     * return Book[]
+     */
+    function viewAllBooks() public view returns ( Book[] memory) {
+        return books;
+    }
+
+    /**
+     * @notice Returns all people that have ever borrowed a given book.
+     * return address[]
+     */
     function viewAllBorrowers() public view returns ( address[] memory) {
         return borrowers;
+    }
+
+    /**
+     * @notice Add the person as a borrower, if it is not already added.
+     * Can only be invoked inside the contract.
+     */
+    function addBorrower(address _borrower) private {
+        if (!isBorrower[_borrower]) {
+            borrowers.push(_borrower);
+            isBorrower[_borrower] = true;
+        }
+
     }
 
 }
